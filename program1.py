4f1a0064496506d9a6bc89116dbc18db30ba8cfa
gurukiran29506 <gurukiranhippargi@gmail.com>
2025-10-18 09:58:06 +0530

    saving file

--- program1.py
+++ program1.py
@@ -2,3 +2,7 @@ num=int(input("Enter a number: "))
 print(f"multiplication table for {num}:")
 for i in range(10):
   print(f"{num} * {i} = {num*i}")
+
+
+num=int(input("Enter a number: "))
+print(f"Cube of {num} is {num**3}")
